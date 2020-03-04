4809477c710507481fc8938a13d6a09fb94d7b10
Xavier Rocamora <janzerker@hotmail.com>
2020-03-04 19:56:41 +0100

    implemented handling of joining and message requests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -15,13 +17,32 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('We have a new connection.');
 
+    // event managing requests to join a room
     socket.on('join', ({ name, room }, callback) => {
         console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
-        const error = true;
+        if(error) return callback(error);
 
+        // if no error then welcome user, announce user joins the room 
+        // to the rest of users in the room and get it in
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the room`});
+        socket.join(user.room);
+
+        callback();  
     });
 
+    // event managing requests from users sending messages
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        console.log(message); //
+        callback();
+    });
+
+    // event managing requests for user disconnection from socket
     socket.on('disconnect', () => {
         console.log('User has left');
     })
